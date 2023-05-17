acf0bc9743e2b35bc31fa587e830dea3eaf89779
Arthur Freitas <arthurrsfreitas@gmail.com>
2023-05-16 23:46:00 -0300

    test: add unit test for category entity

--- CleanArch.Domain/Entities/Category.cs
+++ CleanArch.Domain/Entities/Category.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using CleanArch.Domain.Validation;
+﻿using CleanArch.Domain.Validation;
 
 namespace CleanArch.Domain.Entities
 {
@@ -30,7 +29,7 @@ namespace CleanArch.Domain.Entities
     private void ValidateDomain(string name)
     {
       DomainExceptionValidation.When(string.IsNullOrEmpty(name),
-          "Invalid name.Name is required");
+          "Invalid name. Name is required");
 
       DomainExceptionValidation.When(name.Length < 3,
           "Invalid name, too short, minimum 3 characters");
